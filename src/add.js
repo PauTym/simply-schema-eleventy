b3f78b74c62db386cc92e0a64cf78e20fbec3afd
Paul Tymoschtschuk <p.tymoschtschuk@spedion.de>
2021-10-13 13:29:34 +0200

    Improved code readability

--- src/add.js
+++ src/add.js
@@ -1,15 +1,13 @@
 "use strict";
 
 module.exports = ({ meta }) => {
-  const spaces = 2;
-
   if (meta === "") {
     return "";
   }
 
+  const spaces = 2;
   const json = JSON.stringify(meta, undefined, spaces);
   const jsonSize = json.length - 1;
-
   const finalJson = json.slice(1, jsonSize);
 
   return `,${finalJson}`;
